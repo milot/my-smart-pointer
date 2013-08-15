7ca49f24613eb63bfb30df9799c29953dce8924c
Milot Shala <milot.shala@gmail.com>
2013-08-15 10:54:35 +0200

    Added a ref counted shared pointer and operator*() to smart pointer

---
+++ src/SmartSharedPointer.cpp
@@ -0,0 +1,67 @@
+#include "SmartSharedPointer.h"
+#include "Exceptions.h"
+#include "ReferenceCounter.h"
+#include <iostream>
+
+template<class T>
+SmartSharedPointer<T>::SmartSharedPointer()
+    : data(0), refCounter(0) {
+
+    refCounter = new ReferenceCounter();
+    refCounter->addReference();
+
+}
+
+template<class T>
+SmartSharedPointer<T>::SmartSharedPointer(T *Ptr)
+    : data(Ptr), refCounter(0) {
+
+    refCounter = new ReferenceCounter();
+    refCounter->addReference();
+
+}
+
+template<class T>
+SmartSharedPointer<T>::SmartSharedPointer(const SmartSharedPointer<T>& smartSharedPointer)
+    : data(smartSharedPointer.data), refCounter(smartSharedPointer.refCounter) {
+    refCounter->addReference();
+}
+
+template<class T>
+T* SmartSharedPointer<T>::operator->() {
+    return data;
+}
+
+template<class T>
+T& SmartSharedPointer<T>::operator *() {
+    return *data;
+}
+
+template<class T>
+SmartSharedPointer<T>& SmartSharedPointer<T>::operator=(const SmartSharedPointer<T>& smartSharedPointer) {
+
+    if (this != &smartSharedPointer) { // do not allow self assignment
+
+        // decrement old ref count in order to copy the new one.
+        if (refCounter->releaseReference() == 0) {
+            delete data;
+            delete refCounter;
+        }
+
+        data = smartSharedPointer.data;
+        refCounter = smartSharedPointer.refCounter;
+        refCounter->addReference();
+    }
+
+    return *this;
+}
+
+template<class T>
+SmartSharedPointer<T>::~SmartSharedPointer()
+{
+    if (refCounter->releaseReference() == 0) {
+        delete data;
+        delete refCounter;
+        std::cout << "Destructor of shared ptr" << std::endl << std::flush;
+    }
+}
